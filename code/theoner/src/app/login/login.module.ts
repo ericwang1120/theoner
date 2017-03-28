eb23acf49382e5cfbb9464c90a6157879acba46a
xiaobica <xiaobica@qq.com>
2017-03-29 00:12:06 +1300

    login without css

--- code/theoner/src/app/login/login.module.ts
+++ code/theoner/src/app/login/login.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
+import { AuthenticationService } from '../core/authentication';
 
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, LoginRoutingModule, FormsModule],
   declarations: [LoginComponent],
-  exports: [LoginComponent]
+  exports: [LoginComponent],
+  providers: [AuthenticationService]
 })
 export class LoginModule { }
