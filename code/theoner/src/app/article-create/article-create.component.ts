ced429959e2c203cc2ca57cbc693b39b96ea0d93
xiaobica <xiaobica@qq.com>
2017-04-11 22:34:08 +1200

    article-create front-end

--- code/theoner/src/app/article-create/article-create.component.ts
+++ code/theoner/src/app/article-create/article-create.component.ts
@@ -4,6 +4,8 @@ import { OnInit } from '@angular/core';
 import { Article, ArticleService } from '../core/article'
 import { AlertService } from '../shared/alert/alert.service';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   moduleId: module.id,
   selector: 'article-create',
@@ -14,18 +16,21 @@ import { AlertService } from '../shared/alert/alert.service';
 export class ArticleCreateComponent implements OnInit {
   selectedArticle = new Article();
   articles: Article[];
+  submitFunction = "create";
 
   errorMessage: String;
 
   constructor(
     private articleService: ArticleService,
-    private alertService: AlertService) { }
+    private alertService: AlertService,
+    private modalService: NgbModal) { }
 
   ngOnInit() {
     this.selectedArticle.type = "product";
     this.getArticles('all');
   }
 
+  // Call service funtions
   getArticles(type: string): void {
     this.articleService.getArticles(type)
       .subscribe(
@@ -45,12 +50,47 @@ export class ArticleCreateComponent implements OnInit {
       });
   }
 
+  updateArticle() {
+
+  }
+
+  deleteArticle() {
+
+  }
+  //Used by users on selecting an article
   selectArticle(article: Article): void {
     this.selectedArticle = article;
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-    this.createArticle(this.selectedArticle);
+  //Used by users on clicking OK
+  confirmSubmit() {
+    switch (this.submitFunction) {
+      case ("create"): this.createArticle(this.selectedArticle);
+    }
   }
+
+  onSubmit(e) {
+    if (this.articles.indexOf(this.selectedArticle) !== -1) {
+      this.submitFunction = "update";
+    } else {
+      this.submitFunction = "create";
+    }
+  }
+  //Open delete model
+  modalTitle: string;
+  open(modal, type: string) {
+    this.modalService.open(modal);
+    this.modalTitle = this.setModalTitle(type);
+  }
+
+  setModalTitle(type: any) {
+    let modalTitleMap = {
+      "create": "Create Article",
+      "update": "Update Article",
+      "delete": "Delete Article",
+    };
+    return modalTitleMap[type];
+  }
+
+
 }
